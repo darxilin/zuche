861c3b5636f27fa61422e796eea27d0ccb6c7df7
darxilin <37300923+darxilin@users.noreply.github.com>
2018-04-17 13:52:38 +0800

    0.3.2
    同一版本

--- src/components/sfc.vue
+++ src/components/sfc.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="sfc">
 		<top></top>
+		<mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+  <router-link to="/" slot="left">
+    <mt-button icon="back">返回</mt-button>
+  </router-link>
+  <mt-button icon="more" slot="right"></mt-button>
+</mt-header>
 		顺风车
 	</div>
 </template>
 
 <script>
+	
 	import top from "./common/top";
 	export default {
 		data(){
--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Mint from 'mint-ui';
+import "mint-ui/lib/style.css"
 Vue.use(ElementUI);
+Vue.use(Mint);
 
 Vue.config.productionTip = false
 
