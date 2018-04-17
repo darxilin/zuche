797438583ce9d33cffe15435df6066f3506237e9
darxilin <37300923+darxilin@users.noreply.github.com>
2018-04-17 14:01:23 +0800

    0.3.3
    修改冲突

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Mint from 'mint-ui';
 import "mint-ui/lib/style.css"
-
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
