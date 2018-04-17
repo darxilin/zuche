f42c8ebde2795431e56e38e3324af52d81291d2f
wang jia xing <1191452842@qq.com>
2018-04-17 10:14:39 +0800

    0.1.1

---
+++ src/components/my.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="my">
+		{{name}}
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+			name="李白"
+			}
+		}
+	}
+</script>
+
+<style>
+	#my{
+	 width: 300px;
+	 height: 300px;
+	 background: aqua;
+	}
+</style>
